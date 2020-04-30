3cbfb193ef244b79a04634922e2009e04d30f0dc
Lanboo <549434510@qq.com>
2020-04-30 11:29:16 +0800

    UPDATE 获取输入流被阻塞的问题

--- rpc/rpc-client/src/main/java/com/xych/rpc/client/RpcNetTransport.java
+++ rpc/rpc-client/src/main/java/com/xych/rpc/client/RpcNetTransport.java
@@ -4,6 +4,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import org.apache.commons.io.IOUtils;
+
 import com.xych.rpc.common.rpc.RpcInvocation;
 
 public class RpcNetTransport {
@@ -18,15 +20,21 @@ public class RpcNetTransport {
 
     public Object send(RpcInvocation rpcInvocation) {
         Object result = null;
-        try(Socket socket = new Socket(host, port);
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());) {
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        try(Socket socket = new Socket(host, port)) {
+            oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(rpcInvocation);
             oos.flush();
+            ois = new ObjectInputStream(socket.getInputStream());
             result = ois.readObject();
         }
         catch(Exception e) {
             throw new RuntimeException(e);
+        }
+        finally {
+            IOUtils.closeQuietly(oos);
+            IOUtils.closeQuietly(ois);
         }
         return result;
     }
