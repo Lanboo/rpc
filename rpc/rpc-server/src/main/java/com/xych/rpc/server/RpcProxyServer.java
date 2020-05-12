5b11e6e96c1fac9a76eca11be9c8fdd4352c494a
Lanboo <549434510@qq.com>
2020-05-12 17:30:04 +0800

    ADD rpc netty

--- rpc/rpc-server/src/main/java/com/xych/rpc/server/RpcProxyServer.java
+++ rpc/rpc-server/src/main/java/com/xych/rpc/server/RpcProxyServer.java
@@ -1,73 +1,30 @@
 package com.xych.rpc.server;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 
 public class RpcProxyServer {
-    private ExecutorService executorService = Executors.newCachedThreadPool();
     private final Map<String, Object> serviceMap = new HashMap<>();
+    private int port;
 
-    private Selector selector;
-    private ServerSocketChannel serverSocketChannel;
+    // Boos线程数：CPU核心数*2
+    private final int BIZ_GROUP_SIZE = Runtime.getRuntime().availableProcessors() * 2;
+    // work线程数
+    private final int BIZ_THREAD_SIZE = 100;
+    private final EventLoopGroup BOSS_GROUP = new NioEventLoopGroup(BIZ_GROUP_SIZE);
+    private final EventLoopGroup WORKER_GROUP = new NioEventLoopGroup(BIZ_THREAD_SIZE);
 
     public RpcProxyServer(int port) throws IOException {
-        serverSocketChannel = ServerSocketChannel.open();
-        // 设置通道为非阻塞模式
-        serverSocketChannel.configureBlocking(false);
-        serverSocketChannel.bind(new InetSocketAddress(port));
-        this.selector = Selector.open();
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        this.port = port;
     }
 
     public void publisher() throws Exception {
-        while(true) {
-            // selector.select() 会阻塞
-            // 阻塞n毫秒
-            // 当异步注册事件时，selector.select()会一直阻塞
-            int readyCount = selector.select(1000);
-            if(readyCount == 0) {
-                continue;
-            }
-            Set<SelectionKey> keySet = selector.selectedKeys();
-            CountDownLatch downLatch = new CountDownLatch(keySet.size());
-            Iterator<SelectionKey> keyIter = keySet.iterator();
-            while(keyIter.hasNext()) {
-                SelectionKey key = keyIter.next();
-                keyIter.remove();
-                if(!key.isValid()) {
-                    continue;
-                }
-                if(key.isAcceptable()) {
-                    SocketChannel channel = serverSocketChannel.accept();
-                    // 设置通道为非阻塞模式
-                    channel.configureBlocking(false);
-                    channel.register(selector, SelectionKey.OP_READ);
-                    downLatch.countDown();
-                }
-                else if(key.isReadable()) {
-                    executorService.execute(new ReadProcessorHandler(this.serviceMap, key, downLatch));
-                    // new ReadProcessorHandler(this.serviceMap, key, downLatch).run();
-                }
-                else if(key.isWritable()) {
-                    executorService.execute(new WriteProcessorHandler(key, downLatch));
-                    // new WriteProcessorHandler(key, downLatch).run();
-                }
-            }
-            downLatch.await();
-            // downLatch.await(1000, TimeUnit.MILLISECONDS);
-        }
+
     }
 
     public void put(Object service) {
